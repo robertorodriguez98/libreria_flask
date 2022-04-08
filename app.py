a40efa57667718827eec4514614897d11422e96d
Roberto Rodríguez <krowno.iso@gmail.com>
2022-04-08 20:42:17 +0200

    modificado l libro.html para que compruebe si el libro ha sido publicado

--- app.py
+++ app.py
@@ -14,13 +14,14 @@ def inicio():
 def libro_dinamico(isbn):
     for libro in libreria:
         if libro.get("isbn") == isbn:
+            publicado = libro.get("status")
             nombre = libro.get("title")
             imagen = libro.get("thumbnailUrl")
             paginas = libro.get("pageCount")
             autores = libro.get("authors")
             descripcion = libro.get("shortDescription")
             categorias = libro.get("categories")
-            return render_template("libro.html",nombre=nombre,imagen=imagen,paginas=paginas,autores=autores,descripcion=descripcion,categorias=categorias,isbn=isbn)
+            return render_template("libro.html",nombre=nombre,imagen=imagen,paginas=paginas,autores=autores,descripcion=descripcion,categorias=categorias,publicado=publicado,isbn=isbn)
     return abort(404)
 
 
