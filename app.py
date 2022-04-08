a6ef118ea7284696a0a9491502fef775226c5753
Roberto Rodríguez <krowno.iso@gmail.com>
2022-04-08 10:50:45 +0200

    modificado error en app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,abort
 from funciones import LeerLibreria
 
 libreria = LeerLibreria()
@@ -9,4 +9,10 @@ app = Flask(__name__)
 def inicio():
     return render_template("inicio.html",libreria=libreria)
 
+@app.route('/libro/<isbn>')
+def libro_dinamico(isbn):
+	for libro in libreria:
+		if libro["isbn"] == isbn:
+			return render_template("libro.html",isbn=isbn,libreria=libreria)
+	return abort(404)
 app.run("0.0.0.0",5000,debug=True)
