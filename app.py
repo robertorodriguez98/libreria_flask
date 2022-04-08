18a51f63124ff57ff3cdb4b30530cb4736604208
Roberto Rodríguez <krowno.iso@gmail.com>
2022-04-08 11:52:19 +0200

    añadido a la pagina libro.html la descripcion y el nº de paginas

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import M
 from flask import Flask, render_template, request,abort
 from funciones import LeerLibreria
 
@@ -11,8 +12,14 @@ def inicio():
 
 @app.route('/libro/<isbn>')
 def libro_dinamico(isbn):
-	for libro in libreria:
-		if libro["isbn"] == isbn:
-			return render_template("libro.html",isbn=isbn,libreria=libreria)
-	return abort(404)
+    for libro in libreria:
+        if libro.get("isbn") == isbn:
+            nombre = libro.get("title")
+            imagen = libro.get("thumbnailUrl")
+            paginas = libro.get("pageCount")
+            descripcion = libro.get("shortDescription")
+            return render_template("libro.html",nombre=nombre,imagen=imagen,paginas=paginas,descripcion=descripcion,isbn=isbn)
+    return abort(404)
+
+
 app.run("0.0.0.0",5000,debug=True)
